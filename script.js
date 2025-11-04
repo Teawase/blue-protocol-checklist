0cb61e6da5767c0b52342e4a07148bd2338cc0e9
Teawase <28788037+Teawase@users.noreply.github.com>
2025-11-04 02:57:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -320,27 +320,6 @@
           origin: { x: 0.5, y: -0.5 },
           ticks: 100,
         });
-
-        confetti({
-          particleCount: 500,
-          angle: 180,
-          spread: 360,
-          origin: { x: 0.5, y: 1 },
-        });
-
-        confetti({
-          particleCount: 500,
-          angle: 180,
-          spread: 360,
-          origin: { x: 1, y: 0.5 },
-        });
-
-        confetti({
-          particleCount: 500,
-          angle: 180,
-          spread: 360,
-          origin: { x: 0, y: 0.5 },
-        });
       }
     } else {
       completionMsg.style.display = 'none';
