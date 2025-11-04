4da425b1e6362479057cc2f3bc2196b5266d4635
Teawase <28788037+Teawase@users.noreply.github.com>
2025-11-04 02:24:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -636,9 +636,10 @@
 
 for (let i = localStorage.length - 1; i >= 0; i--) {
   let key = localStorage.key(i);
-  if (key.startsWith("__test_")) {
+  if (key.startsWith("__")) {
     localStorage.removeItem(key);
     console.log(`Removed key: ${key}`);
   }
 }
+
 
