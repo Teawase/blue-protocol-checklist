3d5022b66b737bb27b3fb178e4b850b51cb728e6
Teawase <28788037+Teawase@users.noreply.github.com>
2026-06-29 11:23:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@
     { id: "weekly_sigil_store", label: "🔮 Sigil Exchange Store (Buy x10 Echoes of Ember Sigil Chests)", color: "gold", maxProgress: 1, optional: false },	
     { id: "weekly_reputation_store", label: '📈 Regional Reputation Stores (Buy Will Wish Coins)', color: "gold", maxProgress: 1, optional: false },
 	{ id: "weekly_s2_raids", label: "🛡️ Dragon Shackles or Dreambloom Raids (35220+ Ability Score)", color: "dark_purple", maxProgress: 3, optional: false },
-	{ id: "weekly_s3_raids_easy", label: "🌀 Illusions Raids -Easy- (1970+ Illusion Score | 47650+ Ability Score)", color: "dark_purple", maxProgress: 1, optional: false },
+	{ id: "weekly_s3_raids_easy", label: "🌀 Illusions Raids -Easy- (1970+ Illusion Score | 47650+ Ability Score)", color: "dark_purple", maxProgress: 2, optional: false },
     { id: "weekly_musician_challenges", label: "🎶 Musician (Complete the Weekly Challenges)", color: "grey", maxProgress: 1, optional: true }
   ];
 
